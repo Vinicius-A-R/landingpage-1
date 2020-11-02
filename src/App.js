dfd509d67d2d9f234a44f19a7ad43f2b6abb3c57
Vinicius <vinicius.almeida.r@gmail.com>
2020-11-01 22:26:51 -0300

    FEATURE: add scroll to top

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import { BrowserRouter } from 'react-router-dom';
 
 import Routes from './routes';
 import { Navbar, Footer } from './components';
+import ScrollToTop from './components/ScrollToTop';
 
 import GlobalStyles from './styles/global';
 
 function App() {
   return (
     <BrowserRouter>
+      <GlobalStyles />
+      <ScrollToTop />
       <Navbar />
       <Routes />
       <Footer />
-      <GlobalStyles />
     </BrowserRouter>
   );
 }
