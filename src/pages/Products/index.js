f6b36412c931cd8c30a83900d13bba7d0cb34ba8
Vinicius <vinicius.almeida.r@gmail.com>
2020-11-01 22:17:05 -0300

    STYLED: other pages

---
+++ src/pages/Products/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { InfoSection } from '../../components';
+import { homeObjOne, homeObjTwo } from './Data';
+
+function Products() {
+  return (
+    <>
+      <InfoSection {...homeObjOne} />
+      <InfoSection {...homeObjTwo} />
+    </>
+  );
+}
+
+export default Products;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Homepage from '../pages/Homepage';
+import Services from '../pages/Services';
+import Products from '../pages/Products';
+import SignUp from '../pages/SignUp';
 
 function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={Homepage} />
+      <Route path="/services" component={Services} />
+      <Route path="/products" component={Products} />
+      <Route path="/sign-up" component={SignUp} />
     </Switch>
   );
 }
